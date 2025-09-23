8b5bed3dcb98456242a3ad52b9c8d8c51308947a
Charly Mourglia <1152671+cmourglia@users.noreply.github.com>
2025-09-23 23:02:54 +0200

    Call native functions

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,27 +1,161 @@
+use std::collections::HashMap;
+use std::io::{self, Write};
+
 use crate::lexer::Token;
 use crate::parser::{Expr, Program, Stmt};
 use crate::value::Value;
 
-pub struct Context<W: std::io::Write = std::io::Stdout> {
-    writer: W,
+#[derive(Debug, Clone)]
+pub enum RuntimeError {
+    IoError(String),
+}
+
+impl From<io::Error> for RuntimeError {
+    fn from(error: io::Error) -> Self {
+        // TODO:
+        return RuntimeError::IoError(error.to_string());
+    }
+}
+
+type NativeFunction = fn(&mut Context, &[Value]) -> Result<Option<Value>, RuntimeError>;
+
+#[derive(Debug, Clone)]
+enum Function {
+    Native(NativeFunction),
+    UserDefined { args: Vec<String>, body: Box<Stmt> },
+}
+
+#[derive(Debug)]
+struct Frame {
+    variables: HashMap<String, Value>, // TODO: Get rid of the copies at some point
+    functions: HashMap<String, Function>,
+}
+
+#[derive(Debug)]
+struct FrameStack {
+    frames: Vec<Frame>,
+}
+
+impl FrameStack {
+    pub fn new() -> Self {
+        return FrameStack { frames: vec![] };
+    }
+
+    pub fn push_frame(&mut self) {
+        self.frames.push(Frame {
+            variables: HashMap::new(),
+            functions: HashMap::new(),
+        });
+    }
+
+    pub fn pop_frame(&mut self) {
+        self.frames.pop();
+    }
+
+    // FIXME: Should this return a ref or a clone of the value ?
+    pub fn get_variable(&self, name: &str) -> Option<&Value> {
+        for frame in self.frames.iter().rev() {
+            if let Some(value) = frame.variables.get(&name.to_string()) {
+                return Some(value);
+            }
+        }
+
+        return None;
+    }
+
+    pub fn declare_variable(&mut self, name: &str, value: &Value) {
+        self.frames
+            .last_mut()
+            .unwrap()
+            .variables
+            .insert(name.to_string(), value.clone());
+    }
+
+    pub fn set_variable(&mut self, name: &str, value: &Value) -> Result<(), ()> {
+        for frame in self.frames.iter_mut().rev() {
+            if let Some(v) = frame.variables.get_mut(&name.to_string()) {
+                *v = value.clone();
+                return Ok(());
+            }
+        }
+
+        return Err(());
+    }
+
+    // FIXME: Should this return a ref or a clone of the value ?
+    pub fn get_function(&self, name: &str) -> Option<&Function> {
+        for frame in self.frames.iter().rev() {
+            if let Some(value) = frame.functions.get(&name.to_string()) {
+                return Some(value);
+            }
+        }
+
+        return None;
+    }
+
+    pub fn declare_function(&mut self, name: &str, value: &Function) {
+        self.frames
+            .last_mut()
+            .unwrap()
+            .functions
+            .insert(name.to_string(), value.clone());
+    }
+}
+
+pub struct Context {
+    writer: Box<dyn std::io::Write>,
+    frames: FrameStack,
 }
 
 impl Context {
     pub fn new() -> Self {
-        return Self::with_writer(std::io::stdout());
+        return Self::with_writer(Box::new(std::io::stdout()));
     }
 
     #[allow(unused)]
-    pub fn with_writer<W: std::io::Write>(writer: W) -> Context<W> {
-        return Context { writer };
+    pub fn with_writer(writer: Box<dyn std::io::Write>) -> Context {
+        return Context {
+            writer,
+            frames: FrameStack::new(),
+        };
     }
+}
+
+fn native_print(ctx: &mut Context, values: &[Value]) -> Result<Option<Value>, RuntimeError> {
+    let mut first = true;
+
+    for v in values {
+        if !first {
+            write!(ctx.writer, " ")?;
+        }
+
+        match v {
+            Value::Nil => write!(ctx.writer, "<nil>")?,
+            Value::Number(n) => write!(ctx.writer, "{}", n)?,
+            Value::Boolean(b) => write!(ctx.writer, "{}", b)?,
+        }
+
+        first = false;
+    }
+
+    return Ok(None);
 }
 
 pub fn interpret(prg: &Program) {
     let mut ctx = Context::new();
 
+    interpret_with_context(&mut ctx, prg);
+}
+
+fn interpret_with_context(ctx: &mut Context, prg: &Program) {
+    // Global layer
+    ctx.frames.push_frame();
+
+    ctx.frames
+        .declare_function("print", &Function::Native(native_print));
+
     for stmt in &prg.statements {
-        interpret_stmt(&mut ctx, stmt);
+        interpret_stmt(ctx, stmt);
     }
 }
 
@@ -66,11 +200,43 @@ fn interpret_expr(ctx: &mut Context, expr: &Box<Expr>) -> Value {
             identifier: _,
             value: _,
         } => todo!(),
+
         Expr::Call {
-            callee: _,
-            arguments: _,
-        } => todo!(),
+            ref callee,
+            ref arguments,
+        } => {
+            let function = match ctx.frames.get_function(&callee) {
+                Some(function) => function.clone(),
+                None => todo!("error"),
+            };
+
+            let mut args = Vec::new();
+            for arg in arguments {
+                args.push(interpret_expr(ctx, arg));
+            }
+
+            ctx.frames.push_frame();
+
+            let result = match function {
+                Function::Native(function) => call_native_function(ctx, &function, &args),
+                Function::UserDefined { args, body } => Value::Nil,
+            };
+
+            ctx.frames.pop_frame();
+
+            result
+        }
     }
+}
+
+fn call_native_function(
+    ctx: &mut Context,
+    function: &NativeFunction,
+    arguments: &[Value],
+) -> Value {
+    function(ctx, arguments);
+
+    return Value::Nil;
 }
 
 fn binary_expr(ctx: &mut Context, lhs: &Box<Expr>, rhs: &Box<Expr>, op: &Token) -> Value {
@@ -248,7 +414,75 @@ fn unary_expr(ctx: &mut Context, right: &Box<Expr>, op: &Token) -> Value {
 }
 
 #[cfg(test)]
-mod tests {
+mod program_tests {
+    use std::{
+        io::{self, Write},
+        sync::{Arc, Mutex},
+    };
+
+    use crate::{lexer::tokenize, parser::parse};
+
+    use super::*;
+
+    #[derive(Clone)]
+    struct TestWriter {
+        buffer: Arc<Mutex<Vec<u8>>>,
+    }
+
+    impl TestWriter {
+        fn new() -> Self {
+            return Self {
+                buffer: Arc::new(Mutex::new(vec![])),
+            };
+        }
+
+        fn get_output(&self) -> String {
+            let buffer = self.buffer.lock().unwrap();
+            return String::from_utf8(buffer.clone()).unwrap();
+        }
+    }
+
+    impl Write for TestWriter {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            let mut buffer = self.buffer.lock().unwrap();
+            buffer.extend_from_slice(buf);
+            return Ok(buf.len());
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            return Ok(());
+        }
+    }
+
+    fn test_program_output(input: &str, expected: &str) {
+        let (tokens, errors) = tokenize(input);
+        assert_eq!(errors, vec![]);
+
+        let program = parse(tokens);
+        assert_eq!(program.errors, vec![]);
+        assert_eq!(program.statements.len(), 1);
+
+        let writer = TestWriter::new();
+        let mut ctx = Context::with_writer(Box::new(writer.clone()));
+
+        interpret_with_context(&mut ctx, &program);
+
+        let output = writer.get_output();
+
+        println!("{}", output);
+
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn print_call() {
+        test_program_output("print(42);", "42");
+        test_program_output("print(42, 43, 44);", "42 43 44");
+    }
+}
+
+#[cfg(test)]
+mod expression_tests {
     use crate::{lexer::tokenize, parser::parse};
 
     use super::*;
--- src/value.rs
+++ src/value.rs
@@ -1,7 +1,20 @@
-#[derive(Debug, PartialEq)]
+use std::rc::Rc;
+
+use crate::parser::Stmt;
+
+//#[derive(Debug, PartialEq)]
+//pub enum Cell {
+//    String(String),
+//}
+
+#[derive(Debug, PartialEq, Clone)]
+#[allow(unpredictable_function_pointer_comparisons)]
 pub enum Value {
     Nil,
     Number(f64),
     Boolean(bool),
+    //Cell(Rc<Cell>), // TODO: GC object
+    // NOTE: This should probably not be handled the same way as other values at some point
+    // But this will do for now
     // ...
 }
